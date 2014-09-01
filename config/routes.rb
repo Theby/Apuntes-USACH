537781dfb713de993a4690ff2ce94fc7defd23c9
Esteban Gaete Flores <zoraskiller@gmail.com>
2014-09-01 14:17:32 -0400

    ADD: clase 2 de evaluacion de proyecto

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   # Ruta a los apuntes del ramo Evaluación de Proyectos
   get 'apuntes/ep'
   get 'apuntes/ep/ep_c1' => 'apuntes#ep_c1'
+  get 'apuntes/ep/ep_c2' => 'apuntes#ep_c2'
   # Ruta a los apuntes del ramo Proyecto de Ingeniería de Software
   get 'apuntes/pingeso'
   get 'apuntes/pingeso/pingeso_c1' => 'apuntes#pingeso_c1'
