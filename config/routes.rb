af73e0dc7b52d1337f8bb142138a3ee962a62ac6
Esteban Gaete Flores <zoraskiller@gmail.com>
2014-09-02 20:06:12 -0400

    ADD: clase 3 de pingeso

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   get 'apuntes/pingeso'
   get 'apuntes/pingeso/pingeso_c1' => 'apuntes#pingeso_c1'
   get 'apuntes/pingeso/pingeso_c2' => 'apuntes#pingeso_c2'
+  get 'apuntes/pingeso/pingeso_c3' => 'apuntes#pingeso_c3'
   # Ruta a los apuntes del ramo Comportamiento Humano en el Trabajo
   get 'apuntes/cht'
   get 'apuntes/cht/cht_c4' => 'apuntes#cht_c4'
