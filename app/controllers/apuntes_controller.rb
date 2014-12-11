675e09a99bff8476f4c5d88eac8e3198959cc507
Esteban Gaete <zoraskiller@gmail.com>
2014-12-11 17:54:17 -0300

    ADD: vista generica de entries y metodo privado para encontrar entidades

--- app/controllers/apuntes_controller.rb
+++ app/controllers/apuntes_controller.rb
@@ -1,2 +1,15 @@
 class ApuntesController < ApplicationController
+	before_action :set_apuntes, :only => :show
+
+	# GET /apuntes/:id
+	def show
+	end
+
+	private
+	  # Use callbacks to share common setup or constraints between actions.
+	  def set_apuntes
+	  	@entry = Entry.find(params[:id])
+	  	@publicacion = Publicacion.find(@entry.publicacion_id)
+	  	@expositor = Expositor.find_by :publicacion_id => @entry.publicacion_id
+	  end
 end
