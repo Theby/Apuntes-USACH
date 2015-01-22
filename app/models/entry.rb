c4372723aa6e8be59b347e3b876042833c83f614
Esteban Gaete <zoraskiller@gmail.com>
2015-01-22 19:19:23 -0300

    ADD: url con nombre y anchors con padding

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,4 +3,20 @@ class Entry < ActiveRecord::Base
 
     has_one :entry_siguiente, class_name: "Entry", foreign_key: "entry_anterior_id"
     belongs_to :entry_anterior, class_name: "Entry"
+
+    # Para quitar espacios de los nombres y otros carácteres extraños
+    def slug
+        nombre.downcase.gsub(" ", "_")
+        nombre.gsub("á", "a")
+        nombre.gsub("é", "e")
+        nombre.gsub("í", "i")
+        nombre.gsub("ó", "o")
+        nombre.gsub("ú", "u")
+        nombre.gsub("ñ", "n")
+    end
+
+    # Captura el :id del url antes de pasarsela a otros metodos
+    def to_param
+        "#{id}-#{slug}"
+    end
 end
--- app/models/expositor.rb
+++ app/models/expositor.rb
@@ -1,3 +1,19 @@
 class Expositor < ActiveRecord::Base
   belongs_to :publicacion
+
+  # Para quitar espacios de los nombres y otros carácteres extraños
+  def slug
+      nombre.downcase.gsub(" ", "_")
+      nombre.gsub("á", "a")
+      nombre.gsub("é", "e")
+      nombre.gsub("í", "i")
+      nombre.gsub("ó", "o")
+      nombre.gsub("ú", "u")
+      nombre.gsub("ñ", "n")
+  end
+
+  # Captura el :id del url antes de pasarsela a otros metodos
+  def to_param
+      "#{id}-#{slug}"
+  end
 end
