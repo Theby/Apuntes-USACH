ad9ffe4ed6d5b37fd533a4ab64a03f1dd78d36a1
Esteban Gaete <zoraskiller@gmail.com>
2015-01-31 20:59:12 -0300

    ADD: seed con admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  User.create(email: 'zoraskiller@gmail.com', password: "Stacatto13", password_confirmation: "Stacatto13")
