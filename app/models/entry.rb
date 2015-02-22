b5f0fb09a4affa37260ba681bb10ec4f6b5c6e86
Esteban Gaete <zoraskiller@gmail.com>
2015-02-21 23:35:01 -0300

    FIX: espacios en blanco en la ruta

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -6,19 +6,16 @@ class Entry < ActiveRecord::Base
 
     # Para quitar espacios de los nombres y otros carácteres extraños
     def slug
-        nombre.downcase.gsub(" ", "_")
-        nombre.gsub("á", "a")
-        nombre.gsub("é", "e")
-        nombre.gsub("í", "i")
-        nombre.gsub("ó", "o")
-        nombre.gsub("ú", "u")
-        nombre.gsub("ñ", "n")
-        nombre.gsub("Á", "A")
-        nombre.gsub("É", "E")
-        nombre.gsub("Í", "I")
-        nombre.gsub("Ó", "O")
-        nombre.gsub("Ú", "U")
-        nombre.gsub("Ñ", "N")
+        direccion = nombre
+
+        direccion = direccion.downcase
+        direccion = direccion.gsub("á", "a")
+        direccion = direccion.gsub("é", "e")
+        direccion = direccion.gsub("í", "i")
+        direccion = direccion.gsub("ó", "o")
+        direccion = direccion.gsub("ú", "u")
+        direccion = direccion.gsub("ñ", "n")
+        direccion.delete(' ')
     end
 
     # Captura el :id del url antes de pasarsela a otros metodos
--- app/models/section.rb
+++ app/models/section.rb
@@ -4,13 +4,16 @@ class Section < ActiveRecord::Base
 
     # Para quitar espacios de los nombres y otros carácteres extraños
     def slug
-        nombre.downcase.gsub(" ", "_")
-        nombre.gsub("á", "a")
-        nombre.gsub("é", "e")
-        nombre.gsub("í", "i")
-        nombre.gsub("ó", "o")
-        nombre.gsub("ú", "u")
-        nombre.gsub("ñ", "n")
+        direccion = nombre
+
+        direccion = direccion.downcase
+        direccion = direccion.gsub("á", "a")
+        direccion = direccion.gsub("é", "e")
+        direccion = direccion.gsub("í", "i")
+        direccion = direccion.gsub("ó", "o")
+        direccion = direccion.gsub("ú", "u")
+        direccion = direccion.gsub("ñ", "n")
+        direccion.delete(' ')
     end
 
     # Captura el :id del url antes de pasarsela a otros metodos
